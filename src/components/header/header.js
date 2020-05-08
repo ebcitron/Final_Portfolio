5ea69fd866e8c98ae139dc19bd795fe9f5e4a95d
Eli Citron <ebcitron@gmail.com>
2020-05-08 11:54:15 -0500

    Polishing

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -29,11 +29,11 @@ class Header extends Component{
             <ul className="social">
               {/* <li><a href="#"><i className="fa fa-facebook" /></a></li> */}
               <li><a href="#"><i className="fa fa-twitter" /></a></li>
-              <li><a href="#"><i className="fa fa-google-plus" /></a></li>
-              <li><a href="#"><i className="fa fa-linkedin" /></a></li>
-              {/* <li><a href="#"><i className="fa fa-instagram" /></a></li> */}
+              <li><a href="mailto:ebcitron@gmail.com"><i className="fa fa-google-plus" /></a></li>
+              <li><a href="https://www.linkedin.com/in/elicitron/"><i className="fa fa-linkedin" /></a></li>
+              <li><a href="https://www.github.com/ebcitron/"><i className="fa fa-github" /></a></li>
               {/* <li><a href="#"><i className="fa fa-dribbble" /></a></li> */}
-              <li><a href="#"><i className="fa fa-skype" /></a></li>
+              {/* <li><a href="#"><i className="fa fa-skype" /></a></li> */}
             </ul>
           </div>
         </div>
